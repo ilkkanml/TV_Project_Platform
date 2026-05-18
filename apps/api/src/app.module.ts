b610c9b16a11d48764cf7651a5ca7064a50ffdac
ilkkan <99904094+ilkkanml@users.noreply.github.com>
2026-05-17 23:02:33 -0400

    Register device activation and license controllers

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from "@nestjs/common";
 import { AppVersionController } from "./app-version.controller";
+import { DeviceActivationController } from "./device-activation.controller";
 import { HealthController } from "./health.controller";
+import { LicenseController } from "./license.controller";
 import { RemoteConfigController } from "./remote-config.controller";
 
 @Module({
   imports: [],
-  controllers: [HealthController, AppVersionController, RemoteConfigController],
+  controllers: [
+    HealthController,
+    AppVersionController,
+    RemoteConfigController,
+    DeviceActivationController,
+    LicenseController
+  ],
   providers: []
 })
 export class AppModule {}
