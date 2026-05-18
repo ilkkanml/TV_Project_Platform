075cf4c80ed7a413eeee7ed18d8fb1ea90492568
ilkkan <99904094+ilkkanml@users.noreply.github.com>
2026-05-17 22:34:36 -0400

    Register app version and remote config controllers

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from "@nestjs/common";
+import { AppVersionController } from "./app-version.controller";
 import { HealthController } from "./health.controller";
+import { RemoteConfigController } from "./remote-config.controller";
 
 @Module({
   imports: [],
-  controllers: [HealthController],
+  controllers: [HealthController, AppVersionController, RemoteConfigController],
   providers: []
 })
 export class AppModule {}
